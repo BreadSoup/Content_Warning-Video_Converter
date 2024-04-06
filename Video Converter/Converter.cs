2b57c92bcc07228e21ba26c32c9709a5cb4a85a6
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2024-04-05 20:12:59 -0400

    Initial commit

---
+++ Video Converter/Converter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using BepInEx;
+
+namespace Video_Converter
+{
+    public class Converter : BaseUnityPlugin
+    {
+        public static void Init()
+        {
+            On.CameraRecording.SaveToDesktop += SaveToDesktop;
+        }
+        
+        private static bool SaveToDesktop(On.CameraRecording.orig_SaveToDesktop orig, CameraRecording self, out string videoFileName)
+        {
+            bool originalResult = orig(self, out videoFileName);
+            string ffmpegPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content Warning_Data\\StreamingAssets\\FFmpegOut\\Windows\\ffmpeg.exe");
+            string inputFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), videoFileName);
+            string outputFilePath = Path.ChangeExtension(inputFilePath, ".mp4"); // changes the extension to .mp4
+            string arguments = $"-i {inputFilePath} {outputFilePath}";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = ffmpegPath,
+                Arguments = arguments,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+                process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+            }
+
+            if (File.Exists(outputFilePath))
+            {
+                Plugin.Logger?.LogInfo("Conversion successful deleting WEBM");
+                File.Delete(inputFilePath);
+            }
+            else
+            {
+                Plugin.Logger?.LogError("Conversion failed :(");
+            }
+
+            return originalResult;
+        }
+    }
+}
---
+++ Video Converter/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using JetBrains.Annotations;
+
+namespace Video_Converter
+{
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    [BepInDependency("com.bepinex.plugin.important")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string ModGuid = "breadsoup.Video_Converter";
+        private const string ModName = "Video Converter";
+        private const string ModVersion = "1.1.0";
+        [UsedImplicitly] public new static ManualLogSource? Logger;
+        //private const bool Devmode = true; // Set to false when releasing
+        
+
+        private void Awake()
+        {
+            Converter.Init();
+            //On.ExtractVideoMachine.CheckState += CheckState; kinda useless and broken for now
+        }
+        
+        /*private void CheckState(On.ExtractVideoMachine.orig_CheckState orig, ExtractVideoMachine self)
+        {
+            if (Devmode)
+            {
+                SurfaceNetworkHandler.ReturnFromLostWorld();
+                //need to figure out a way to get the video extractor to open and not break
+                UploadVideoStation.FindObjectsOfType<UploadVideoStation>()[0].Unlock();
+            }
+            orig(self);
+        }*/
+    }
+}
