bdcf882d8f24f9b4621358cbfef0fafb20c3202b
BreadSoup <breadsoup64@gmail.com>
2024-04-05 23:24:30 -0400

    Theoretical custom save location support

---
+++ Video Converter/Compatibility.cs
@@ -0,0 +1,20 @@
+﻿namespace Video_Converter
+{
+    public static class CustomLocationCompatibility
+    {
+        private static bool? _enabled;
+
+        public static bool Enabled
+        {
+            get
+            {
+                if (_enabled == null)
+                {
+                    _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.ramune.CustomVideoSaveLocation");
+                }
+
+                return (bool)_enabled;
+            }
+        }
+    }
+}
--- Video Converter/Converter.cs
+++ Video Converter/Converter.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using BepInEx;
+using System.Runtime.CompilerServices;
+using Ramune.CustomVideoSaveLocation;
 
 namespace Video_Converter
 {
-    public class Converter : BaseUnityPlugin
+    public static class Converter
     {
+        
         public static void Init()
         {
+            Plugin.Logger?.LogInfo("Converter initialized");
             On.CameraRecording.SaveToDesktop += SaveToDesktop;
+            
         }
         
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
         private static bool SaveToDesktop(On.CameraRecording.orig_SaveToDesktop orig, CameraRecording self, out string videoFileName)
         {
+            Plugin.Logger?.LogInfo("Beginning conversion process");
             bool originalResult = orig(self, out videoFileName);
             string ffmpegPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content Warning_Data\\StreamingAssets\\FFmpegOut\\Windows\\ffmpeg.exe");
-            string inputFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), videoFileName);
+            string inputFilePath;
+            if (CustomVideoSaveLocation.Path.Value != "Desktop" && CustomVideoSaveLocation.Path.Value != null && CustomLocationCompatibility.Enabled)
+            {
+                inputFilePath = CustomVideoSaveLocation.Path.Value;
+                Plugin.Logger?.LogInfo("Custom path detected");
+            }
+            else
+            {
+                inputFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), videoFileName);
+                Plugin.Logger?.LogInfo("Custom path not detected, using desktop");
+            }
             string outputFilePath = Path.ChangeExtension(inputFilePath, ".mp4"); // changes the extension to .mp4
             string arguments = $"-i {inputFilePath} {outputFilePath}";
 
@@ -49,5 +65,7 @@ namespace Video_Converter
 
             return originalResult;
         }
+        
+        
     }
 }
