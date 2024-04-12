99b0b8323c5da31e0b113018745afcf5b8b60ddc
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2024-04-11 21:09:30 -0400

    Infinite Camera

--- Video Converter/Plugin.cs
+++ Video Converter/Plugin.cs
@@ -1,36 +1,23 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
 
-namespace Video_Converter
+namespace Infinite_Camera
 {
     [BepInPlugin(ModGuid, ModName, ModVersion)]
-    [BepInDependency("com.ramune.CustomVideoSaveLocation", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
-        private const string ModGuid = "breadsoup.Video_Converter";
-        private const string ModName = "Video Converter";
-        private const string ModVersion = "1.1.2";
-        internal new static ManualLogSource? Logger { get; private set; }
-        private const bool Devmode = false; // Set to false when releasing
+        private const string ModGuid = "breadsoup.Infinite_Camera";
+        private const string ModName = "Infinite Camera";
+        private const string ModVersion = "1.0.0";
 
         private void Awake()
         {
-            Logger = base.Logger;
-            
-            Converter.Init();
-            On.SurfaceNetworkHandler.RPCM_StartGame += RPCM_StartGame;
+            On.VideoCamera.Update += VideoCamera_Update;
         }
 
-
-        private void RPCM_StartGame(On.SurfaceNetworkHandler.orig_RPCM_StartGame orig, SurfaceNetworkHandler self)
+        private void VideoCamera_Update(On.VideoCamera.orig_Update orig, VideoCamera self)
         {
-            if (Devmode)
-            {
-                SurfaceNetworkHandler.ReturnFromLostWorld();
-                ExtractVideoMachine.FindObjectsOfType<ExtractVideoMachine>()[0].Awake();
-                UploadVideoStation.FindObjectsOfType<UploadVideoStation>()[0].Awake();
-            }
             orig(self);
+            self.m_recorderInfoEntry.timeLeft = self.m_recorderInfoEntry.maxTime;
         }
     }
 }
